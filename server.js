03f93d7761b4ea5276767d1fad1c67b9872f1ed1
Imran <imran2018wahid@gmail.com>
2021-03-04 22:54:14 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const ejs=require('ejs');
+const expressLayout=require('express-ejs-layouts');
+const path=require('path');
+const app=express();
+
+
+app.get('/',(req,res)=>
+{
+    res.render('home');
+});
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+const port=process.env.PORT || 3000;
+app.listen(port,()=>
+{
+    console.log(`Listening on port ${port}`);
+})
