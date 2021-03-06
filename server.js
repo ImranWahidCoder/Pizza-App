bb2aa6701c9371457bd5b88c4700f4177cd8aa3a
Imran <imran2018wahid@gmail.com>
2021-03-06 18:44:48 +0530

    second commit

--- server.js
+++ server.js
@@ -4,16 +4,28 @@ const expressLayout=require('express-ejs-layouts');
 const path=require('path');
 const app=express();
 
+app.use(express.static('public'));
+// app.use(expressLayout);
 
 app.get('/',(req,res)=>
 {
     res.render('home');
 });
-app.use(express.static('public'));
-app.use(expressLayout);
+app.get('/cart',(req,res)=>
+{
+    res.render('customers/cart');
+});
+app.get('/login',(req,res)=>
+{
+    res.render('auth/login');
+});
+app.get('/register',(req,res)=>
+{
+    res.render('auth/register');
+});
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3300;
 app.listen(port,()=>
 {
     console.log(`Listening on port ${port}`);
